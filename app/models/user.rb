8c96f4b38aaed0768c59263cc14b88aed479c696
hashimoto-masataka <115071516+hashimoto-masataka@users.noreply.github.com>
2022-12-27 09:26:21 +0000

    いいね多い順に投稿

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@ class User < ApplicationRecord
   has_many :rooms, through: :user_rooms
   has_many :chats
   has_many :user_rooms
+  
+  has_many :view_counts, dependent: :destroy
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
